5173e0746d3725a47cd797184f14959901933959
souzap371 <souzap371@gmail.com>
2023-08-01 21:32:45 -0300

    criacao de CRUD e conexao com banco de dados e rotas HTTP

---
+++ rest-with-spring-boot-and-java-/src/main/java/br/com/pablo/services/PersonServices.java
@@ -0,0 +1,68 @@
+package br.com.pablo.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.pablo.exceptions.ResourceNotFoundException;
+import br.com.pablo.model.Person;
+import br.com.pablo.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+		
+		return repository.findAll();
+		
+	}
+
+	public Person findById(Long id) {
+		
+		logger.info("Finding one person!");
+				
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+
+
+	public Person create(Person person) {
+		
+		logger.info("Creating one person!");
+		//acessa base de dados e recupera os dados
+		return repository.save(person);
+	}
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+		
+		var entity = repository.findById(person.getId())
+		.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
+	}
+	public void delete(Long id) {
+		
+		logger.info("Deleting one person!");
+		//acessa base de dados e recupera os dados
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+		
+}
